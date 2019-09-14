9107ad2467439357b441394f1e50cbd0613cc5ee
cameronshum <cameronshum@hotmail.com>
2019-09-14 05:59:15 -0400

    Added wifi enabling capabilities

--- AndroidApp/app/src/main/java/com/example/placeholder/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/placeholder/MainActivity.java
@@ -1,43 +1,48 @@
 package com.example.placeholder;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "placeHolder";
+//    public static final String TAG = "placeHolder";
+//
+//    private TextView mTextView;
+//    private NfcAdapter mNfcAdapter;
 
-    private TextView mTextView;
-    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+//        mTextView = (TextView) findViewById(R.id.textView_explanation);
+//
+//        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+//
+//        if (mNfcAdapter == null) {
+//            Toast.makeText(this, "No NFC", Toast.LENGTH_LONG).show();
+//            finish();
+//            return;
+//        }
+//
+//        if (!mNfcAdapter.isEnabled()) {
+//            mTextView.setText("NFC is disabled.");
+//        } else {
+//            mTextView.setText(R.string.explanation);
+//        }
+//
+////        handleIntent(getIntent());
+//    }
+//
+//    private void handleIntent(Intent intent) {
+//        //DSADSADSADASDSAD
+//    }
 
-        mTextView = (TextView) findViewById(R.id.textView_explanation);
-
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
-        if (mNfcAdapter == null) {
-            Toast.makeText(this, "No NFC", Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
-
-        if (!mNfcAdapter.isEnabled()) {
-            mTextView.setText("NFC is disabled.");
-        } else {
-            mTextView.setText(R.string.explanation);
-        }
-
-        handleIntent(getIntent());
-    }
-
-    private void handleIntent(Intent intent) {
-        //DSADSADSADASDSAD
-    }
-
-    final WifiConnector connector = new WifiConnector();
-}
+    final WifiConnector connector = new WifiConnector(this);
+}}
